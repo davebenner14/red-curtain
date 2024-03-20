79617edf515dcc2c393b4bf93012362f0fa909ed
David Benner <davebenner14@gmail.com>
2024-03-20 09:18:34 -0400

    Add files and basic code

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var curtainLeft = document.getElementById('curtainLeft');
+    var curtainRight = document.getElementById('curtainRight');
+
+    setTimeout(function() {
+        curtainLeft.style.transform = 'translateX(-100vw)';
+        curtainRight.style.transform = 'translateX(100vw)';
+    }, 1000);
+});
